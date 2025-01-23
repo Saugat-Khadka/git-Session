ceb46fa4c80e51f0d779ba896dad9795c6bc2b59
Saugat-Khadka <ksaugat140@gmail.com>
2025-01-23 08:47:02 +0545

    myfile

---
+++ Script.js
@@ -0,0 +1,10 @@
+/* document.write("Hello, world!<br>");
+document.write("(!o.o!)<br>");
+const a=10;const b=20; const sum=a+b;
+document.write("The sum of "+a+" and "+b+" is "+sum+".<br>");
+d = new Date();
+alert(d);
+var c=prompt("Enter your name","your name"); */
+function myFunction(){
+    alert("Hello,welcome to our website!");
+}
---
+++ jan9.js
@@ -0,0 +1,11 @@
+var human = new Object();
+
+let human = {
+    age:21,
+    name : 'sabeen'
+};
+
+human.complexion = 'olive';
+console.log(human.age);
+human.age='22';
+console.log(human.age);
